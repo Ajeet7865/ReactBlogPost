73a22406362e731015afdb6898717464f98d0978
Ajeet <48230363+Ajeet7865@users.noreply.github.com>
2024-11-10 22:21:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import BlogHome from './Components/Projects/MegaBlog/Home.jsx'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <><Navbar /><BlogHome />  </>,
+    element: <><Navbar /><BlogHome /> </>,
     children: [
       {
         path: "mapfunction",
